5518cc44be8af3e5d4d81f65fa16c5ad094352e0
Miraz PC <abdullahmiraz2015@gmail.com>
2024-05-02 00:00:30 +0600

    updated the modal functions

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import Modal from "./Modal";
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   return (
     <div>
-      <button className="btn btn-primary w-full">
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
         Add New Task <FaPlus size={13} />{" "}
       </button>
+
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        Modal for add todo
+      </Modal>
     </div>
   );
 };
